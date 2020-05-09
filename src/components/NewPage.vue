61a77f14df3b238b83f8b5e6256d680965f06025
haru3613 <qaw8975889@gmail.com>
2020-05-09 15:54:09 +0800

    My first learning

---
+++ src/components/NewPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>Hello new page.</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NewPage',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
